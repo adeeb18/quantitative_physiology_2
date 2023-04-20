6c8d18d5342b67e0a72c5e625d0ea0b35b3c8af5
Adeeb Rashid <adeeb.rashid@ufl.edu>
2023-04-20 19:32:27 -0400

    update

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 #Constants
 
@@ -14,6 +15,12 @@ n = 1
 # Cutnell, John & Johnson, Kenneth. Physics, Fourth Edition. Wiley, 1998: 308.
 rho = 1060
 
+# Pressure
+p = 1
+
+# Sin of angle for vessel
+sintheta = math.sin(45)
+
 # acceleration due to gravity (m/s^2)
 g_start =0
 g_step =.01
@@ -23,7 +30,7 @@ g=np.arange(g_start, g_stop, g_step)
 
 
 #Shear Stress
-tau = m * (g) ** n
+tau = m * np.abs(p - np.multiply(np.multiply(g,rho), sintheta)) ** n
 
 #Plotting
 fig, ax = plt.subplots()
