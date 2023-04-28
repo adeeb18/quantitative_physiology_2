dff1c6dfc7b5b232632423c3b1a96c038fbd19d4
vedantr02 <122630293+vedantr02@users.noreply.github.com>
2023-04-28 07:23:46 -0400

    Update Project2_Code.py

--- Project2_Code.py
+++ Project2_Code.py
@@ -1,10 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
+from matplotlib import image as mpimg
 import math
 
 #Constants
 plt.rcParams["font.family"] = "Times New Roman"
+plt.rcParams["font.size"] = 10
 
 # m constant for blood as a power law fluid (Pa*s^n / m^2)
 m = 0.01615
@@ -64,8 +66,8 @@ tau_NW = (np.abs(((p/l) - np.multiply(np.multiply(g,rho), sintheta)) * (r / 2)))
 plt.figure()
 gs = GridSpec(nrows=2, ncols=2)
 plt.subplot(gs[0,0])
-plt.xlabel("Acceleration due to Gravity (m/s^2)")
-plt.ylabel("Wall Shear Stress (Pa)")
+plt.xlabel("Acceleration due to Gravity (m/s^2)", fontsize = 11)
+plt.ylabel("Wall Shear Stress (Pa)", fontsize = 11)
 plt.title("Gravity-Induced Changes in Wall Shear Stress in Generalized Coronary Artery")
 plt.plot(g, tau_PL, linewidth=2.0, color='r', label='Power Law Fluid (Shear Thinning)')
 plt.plot(g, tau_NW, linewidth=2.0, color='b', label='Newtonian Fluid')
@@ -75,7 +77,7 @@ plt.grid()
 #plt.plot(g, tau_nog, linewidth=2.0, color='y')
 plt.legend(loc='lower right')
 
-#plt.show()
+
 
 # Coronary arteries length (m)
 l = .030 
@@ -98,8 +100,8 @@ tau_space = m * np.abs(((p/l) - np.multiply(np.multiply(g_space,rho), sintheta))
 
 #plt.figure()
 plt.subplot(gs[1,:])
-plt.xlabel("Vessel Radius (mm)")
-plt.ylabel("Wall Shear Stress (Pa)")
+plt.xlabel("Vessel Radius (mm)", fontsize = 11)
+plt.ylabel("Wall Shear Stress (Pa)", fontsize = 11)
 plt.title("Gravity-Induced Changes in Wall Shear Stress along Vessel Radius in Shear-Thinning Blood Model")
     
 gravity_comp = [9.8e-6, 3.3, 6.6, 9.8]
@@ -120,8 +122,9 @@ plt.legend()
 # plt.plot(r_variable, tau_takeoff, linewidth=2.0, color='g', label='takeoff, g=9.8 * 4')
 # plt.plot(r_variable, tau_space, linewidth=2.0, color='b', label='space, g=9.8/1000000')
 # plt.legend()
-
-#plt.show()
+# Image = mpimg.imread("Radius Figure.png")
+# plt.imshow(Image)
+# plt.show()
 
 # Variable angles coronary arteries (m)
 theta_start = -90
@@ -140,8 +143,8 @@ sintheta_variable = np.sin(theta_variable * np.pi/180.)
 
 #plt.figure()
 plt.subplot(gs[0,1])
-plt.xlabel("Angle \u03F4 (Redians)")
-plt.ylabel("Wall Shear Stress (Pa)")
+plt.xlabel("Angle \u03F4 (Redians)", fontsize = 11)
+plt.ylabel("Wall Shear Stress (Pa)", fontsize = 11)
 plt.title("Gravity-Induced Changes in Wall Shear Stress at Different Angles in Shear-Thinning Blood Model")
 
 gravity_comp = [9.8e-6, 3.3, 6.6, 9.8]
@@ -160,4 +163,28 @@ plt.grid()
 # plt.plot(theta_variable, tau_takeoff, linewidth=2.0, color='g', label='takeoff, g=4*ge')
 # plt.plot(theta_variable, tau_space, linewidth=2.0, color='b', label='space, g=ge/100000')
 plt.legend(loc = 'lower right')
+position = plt.subplot(gs[1,:]).get_position()
+new_position = [position.x0, position.y0-0.04, position.width, position.height]
+plt.subplot(gs[1,:]).set_position(new_position)
+plt.show()
+
+# Controls Analysis
+g_control_start = 10
+g_control_end = 20
+g_control_step = 0.01
+
+g_control = np.arange(g_control_start, g_control_step, g_control_step)
+
+tau_PL_control = (m * np.abs(((p/l) - np.multiply(np.multiply(g_control,rho), sintheta)) * (r / 2)) ** n) # Power Law Shear Stress Control Analysis
+tau_NW_control = (np.abs(((p/l) - np.multiply(np.multiply(g_control,rho), sintheta)) * (r / 2)))/280 # Newtonian Shear Stress Analysis
+
+plt.figure()
+plt.xlabel("Acceleration due to Gravity (m/s^2)", fontsize = 11)
+plt.ylabel("Wall Shear Stress (Pa)", fontsize = 11)
+plt.title("Gravity-Induced Changes in Wall Shear Stress in Generalized Coronary Artery: Control Analysis")
+plt.plot(g_control, tau_PL_control, linewidth=2.0, color='r', label='Power Law Fluid (Shear Thinning)')
+plt.plot(g_control, tau_NW_control, linewidth=2.0, color='b', label='Newtonian Fluid')
+plt.axhline(y = 0.60, color='k', linestyle='--')
+plt.grid()
+plt.legend(loc='lower right')
 plt.show()
